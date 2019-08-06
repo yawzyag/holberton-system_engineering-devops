9fda1208e0b9ef67e113deb019997f37a0d7471f
yawzyag <yesid.yag@gmail.com>
2019-08-06 16:00:05 +0000

    [Fix] addin  format

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 # cosito puppy
-
-file_line { 'Turn off passwd auth':
-  ensure => created,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => 'PasswordAuthentication',
+file { '/etc/ssh/ssh_config':
+      ensure => present,
 }
-file_line { 'Declare identity file':
-  ensure => created,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentifyFile ~/.ssh/holberton',
-  match  => 'IdentifyFile',
-}
+      -> file_line { 'Turn off passwd auth':
+          ensure => present,
+      path       => '/etc/ssh/ssh_config',
+      line       => 'PasswordAuthentication no',
+      match      => 'PasswordAuthentication',
+      }
+      -> file_line { 'Declare identity file':
+          ensure => present,
+      path       => '/etc/ssh/ssh_config',
+      line       => 'IdentifyFile ~/.ssh/holberton',
+      match      => 'IdentifyFile',
+      }
