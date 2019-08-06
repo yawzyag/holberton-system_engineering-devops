1ed301a4e3da9c11874f18b29f45d46d64efea6d
yawzyag <yesid.yag@gmail.com>
2019-08-06 16:06:10 +0000

    [Fix] ^^

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 file { '/etc/ssh/ssh_config':
       ensure => present,
 }
-      -> file_line { 'Turn off passwd auth':
-          ensure => present,
-      path       => '/etc/ssh/ssh_config',
-      line       => 'PasswordAuthentication no',
-      match      => 'PasswordAuthentication',
-      }
-      -> file_line { 'Declare identity file':
-          ensure => present,
-      path       => '/etc/ssh/ssh_config',
-      line       => 'IdentifyFile ~/.ssh/holberton',
-      match      => 'IdentifyFile',
-      }
+-> file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication',
+}
+-> file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/holberton',
+  match  => '^IdentifyFile',
+}
