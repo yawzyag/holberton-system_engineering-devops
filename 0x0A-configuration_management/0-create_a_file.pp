b770f1738df7224db7a221bfb845cc3a05c7a5d4
yawzyag <yesid.yag@gmail.com>
2019-08-06 01:45:30 +0000

    [Fix] puppy style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # cosito puppy
 file { '/tmp/holberton':
       ensure  => 'present',
+      path    => '/tmp/holberton',
       owner   => 'www-data',
       group   => 'www-data',
       mode    => '0744',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
 # cosito pkill
 exec { 'killmenow':
-     command => '/usr/bin/pkill -f killmenow',
+      command => '/usr/bin/pkill -f killmenow',
 }
