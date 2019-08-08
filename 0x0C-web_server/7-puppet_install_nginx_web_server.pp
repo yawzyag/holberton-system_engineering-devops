48c40c418ba8db7a6ee835a1ffa53ee3ae79eb23
Yesid <31510821+yawzyag@users.noreply.github.com>
2019-08-07 19:55:03 -0500

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,14 +16,14 @@ file { '/etc/nginx/sites-available/default':
   path   => '/etc/nginx/sites-available/default',
   line   => '    listen 80 default_server;
          location /redirect_me {
-                  return 301 https://www.youtube.com/;
+                  return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
           }',
   match  => 'listen 80 default_server',
+}
+exec { 'restart':
+      command => '/usr/sbin/service nginx restart',
 }
 service { 'nginx':
   ensure => running,
   enable => true,
-}
-exec { 'restart':
-      command => '/usr/sbin/service nginx restart',
 }
