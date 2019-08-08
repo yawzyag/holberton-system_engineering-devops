0b931ab6e2a636de38c507d51db79ca2b39bb032
yawzyag <yesid.yag@gmail.com>
2019-08-08 00:27:57 +0000

    [Fix] aadinf ile

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# soy un comentario cool
+package {'nginx':
+ensure => present,
+
+}
+file { '/tmp/holberton':
+ensure        => 'present',
+      path    => '/var/www/html/index.html',
+      content => 'Holberton School',
+}
+
+file { '/etc/nginx/sites-available/default':
+      ensure => present,
+}
+-> file_line { 'Edit redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => '    listen 80 default_server;
+         location /redirect_me {
+                  return 301 https://www.youtube.com/;
+          }',
+  match  => 'listen 80 default_server',
+}
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
