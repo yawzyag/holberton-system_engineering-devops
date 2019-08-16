fdcf5b124d414981567bccfebb5f27e8fbe03c34
yawzyag <yesid.yag@gmail.com>
2019-08-16 20:49:36 +0000

    [Feature] puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,22 @@
+# soy un comentario cool
+package {'nginx':
+ensure => 'installed',
+}
+-> file { '/etc/nginx/sites-available/default':
+            ensure => present,
+}
+-> file_line { 'Edit redirect':
+            ensure  => present,
+            path    => '/etc/nginx/sites-available/default',
+              line  => '       location / {
+	      	   add_header X-Served-By 766-web-02;',
+              match => '^\tlocation / {',
+}
+-> exec { 'restart':
+            command => '/usr/sbin/service nginx restart',
+}
+service { 'nginx':
+          ensure   => running,
+            enable => true,
+}
+
