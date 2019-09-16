38b82c6dc9c29af52fe0ec076ab4937ffcfafad4
yawzyag <yesid.yag@gmail.com>
2019-09-16 17:52:12 +0000

    [Feature] adding 1 file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""getting info from an api"""
+import csv
+import requests as r
+from sys import argv
+
+
+if __name__ == "__main__":
+    """exectute req"""
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos = r.get(todo_url)
+    users = r.get(users_url)
+    done, total, tasks, compl = 0, 0, [], []
+    for x in todos.json():
+        if x.get("userId") == int(argv[1]):
+            total += 1
+            if x.get("completed") == True:
+                done += 1
+            tasks.append(x.get("title"))
+            compl.append(x.get("completed"))
+    for y in users.json():
+        if y.get("id") == int(argv[1]):
+            user = y.get("username")
+            my_id = y.get("id")
+
+    actual, to_csv, items = 0, [], []
+    for task in tasks:
+        items.append(my_id)
+        items.append(user)
+        items.append(compl[actual])
+        items.append(task)
+        to_csv.append(items)
+        items = []
+        actual += 1
+
+    csv.register_dialect('myDialect',
+                         quoting=csv.QUOTE_ALL,
+                         skipinitialspace=True)
+    with open('employee_file.csv', mode='w') as employee_file:
+        task_writer = csv.writer(employee_file, dialect='myDialect')
+        for info in to_csv:
+            task_writer.writerow(info)
