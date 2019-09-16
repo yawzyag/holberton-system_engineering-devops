6546894ed9020afc03a4f980ebf554f6040e229a
yawzyag <yesid.yag@gmail.com>
2019-09-16 19:52:15 +0000

    [Feature] adding file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""getting info from an api"""
+import json
+import requests as r
+from sys import argv
+
+
+if __name__ == "__main__":
+    """exectute req to csv"""
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        int(argv[1]))
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        int(argv[1]))
+    todos = r.get(todo_url).json()
+    users = r.get(users_url).json()
+    user = users.get("username")
+    user_id = str(users.get("id"))
+    response = {}
+    user_todo = []
+    temp = {}
+    for todo in todos:
+        temp.update({"task": todo.get("title")})
+        temp.update({"completed": todo.get("completed")})
+        temp.update({"username": user})
+        user_todo.append(temp)
+        temp = {}
+
+    response = {user_id: user_todo}
+    name = "{}.json".format(user_id)
+    with open(name, mode='w') as file:
+        json.dump(response, file)
