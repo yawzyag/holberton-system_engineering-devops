22cb54ee852e13b757e9ebf417594d13f4fa7f8e
yawzyag <yesid.yag@gmail.com>
2019-09-19 19:47:28 +0000

    [Feature] adding recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests as r
+"""count number of subscribers"""
+
+
+def recurse(subreddit, hot_list=[], after="null"):
+    """Get number of subscribers"""
+    url = "https://www.reddit.com/r/{}.json?sort=hot&after={}".format(
+        subreddit, after)
+    headers = {'User-Agent': 'Yesid'}
+    try:
+        subscribers = r.get(url, headers=headers,
+                             allow_redirects=False).json()
+        data = subscribers.get("data")
+        after = subscribers.get("data").get("after")
+        if after != None:
+            hot_list += data["children"]
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    except:
+        return None
