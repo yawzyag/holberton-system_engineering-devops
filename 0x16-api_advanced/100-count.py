412c71b27b3d22a13b8ff4626998dd33f8f8c807
yawzyag <yesid.yag@gmail.com>
2019-09-20 01:17:16 +0000

    [Fix] hasjklñd

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,35 +3,44 @@ import requests as r
 """count number of subscribers"""
 
 
-def count_words(subreddit, word_list, lista=[], dicty={}, after="null"):
+def recurse(subreddit, hot_list=[], after="null"):
+    """Get number of subscribers"""
+    url = "https://www.reddit.com/r/{}.json?sort=hot&after={}&limit=100"\
+        .format(subreddit, after)
+    headers = {'User-Agent': 'Yesid'}
+    try:
+        subscribers = r.get(url, headers=headers,
+                            allow_redirects=False).json()
+        data = subscribers.get("data")
+        after = subscribers.get("data").get("after")
+        hot_list += [story.get("data")['title'] for story in data['children']]
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    except:
+        pass
+
+
+def count_words(subreddit, word_list, lista=None, dicty={},
+                count=0):
     """Get number of subscribers"""
 
-    if lista == [] or after is not None:
-        url = "https://www.reddit.com/r/{}.json?sort=hot&after={}&limit=100"\
-            .format(subreddit, after)
-        headers = {'User-Agent': 'Yesid'}
-        try:
-            subscribers = r.get(url, headers=headers,
-                                allow_redirects=False).json()
-            data = subscribers.get("data")
-            after = subscribers.get("data").get("after")
-            lista += [story.get("data")['title'] for story in data['children']]
-            if after is not None:
-                count_words(subreddit, word_list, lista, dicty, after)
-        except:
-            pass
     word_list = list(set(word_list))
-    if after is None:
-        count = 0
-        if word_list != [] and lista:
+    if lista is None:
+        lista = recurse(subreddit)
+    if lista and len(lista) > 0:
+        if word_list != []:
             for item in lista:
-                if word_list[0].lower() in item.lower().split():
-                    if word_list[0] in dicty.keys():
-                        count = dicty[word_list[0]]
+                item = item.lower()
+                if word_list[-1].lower() in item.split():
+                    if word_list[-1] in dicty.keys():
+                        count = dicty[word_list[-1]]
+                    else:
+                        count = 0
                     count += 1
-                    dicty[word_list[0]] = count
-            word_list.pop(0)
-            count_words(subreddit, word_list, lista, dicty, after)
+                    dicty.update({word_list[-1]: count})
+            word_list.pop(-1)
+            count_words(subreddit, word_list, lista, dicty, count)
         else:
             for key in sorted(dicty.items(), key=lambda kv: (-kv[1], kv[0])):
                 print("{}: {}".format(key[0], key[1]))
