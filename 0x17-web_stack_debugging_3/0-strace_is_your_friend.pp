3a2bfaffad1b7c6aa48b1464d628019fbf9d575d
Yesid <31510821+yawzyag@users.noreply.github.com>
2019-10-02 11:47:27 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet manifest to fix termination file
 exec { 'fix-wordpress':
-  path    => '/bin:/usr/bin:/usr/sbin',
+  path    => '/bin:/usr/bin',
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
